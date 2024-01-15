3e3130a2cf338defb3de71a1256e228ef4a5803e
Germán Orellano <gorellano93@gmail.com>
2024-01-15 11:05:12 -0300

    sprint4

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 import express from "express";
 import router from "./src/routers/index.router.js";
+import errorHandler from "./src/middlewares/errorHandler.mid.js";
+import pathHandler from "./src/middlewares/pathHandler.mid.js";
+import morgan from "morgan";
 
 const server = express();
-
 const PORT = 8080;
 const ready = () => {
   console.log(`Express server listening on port: ${PORT}`);
 };
+server.listen(PORT, ready);
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-
+server.use(morgan("dev"));
 server.use("/", router);
-
-server.listen(PORT, ready);
+server.use(errorHandler);
+server.use(pathHandler);
